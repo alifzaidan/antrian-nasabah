790ffdd05b341c04136cec4fee5f260b6917518f
ThoriqSlebew <thoriqkhoirkhoir537@gmail.com>
2024-07-10 10:06:10 +0700

    split table

--- resources/views/panggil-antrian-detail.blade.php
+++ resources/views/panggil-antrian-detail.blade.php
@@ -26,7 +26,7 @@
     </header>
 
     <main>
-        <div class="grid sm:grid-cols-4 grid-cols-2 gap-10 my-10">
+        <div class="grid sm:grid-cols-4 grid-cols-2 gap-5 my-10">
             <div class="bg-primary bg-opacity-10 rounded-2xl shadow-lg p-4">
                 <div class="bg-white rounded-2xl flex items-center justify-center gap-4 flex-col p-6 h-full">
                     <h1 class="text-lg font-poppins text-center">Jumlah Antrean</h1>
@@ -41,7 +41,7 @@
                     <h1 class="text-lg font-poppins text-center">Antrean Sekarang</h1>
                     <div class="flex gap-4">
                         <img src="{{asset('icons/people-2.svg')}}" alt="Jumlah Antrian" class="w-12">
-                        <h2 class="text-5xl font-bold font-poppins text-center text-tertiary">TL005</h2>
+                        <h2 class="text-5xl font-bold font-poppins text-center text-tertiary">TL004</h2>
                     </div>
                 </div>
             </div>
@@ -50,7 +50,7 @@
                     <h1 class="text-lg font-poppins text-center">Antrean Selanjutnya</h1>
                     <div class="flex gap-4">
                         <img src="{{asset('icons/people-3.svg')}}" alt="Jumlah Antrian" class="w-12">
-                        <h2 class="text-5xl font-bold font-poppins text-center text-secondary">TL006</h2>
+                        <h2 class="text-5xl font-bold font-poppins text-center text-secondary">TL005</h2>
                     </div>
                 </div>
             </div>
@@ -65,50 +65,80 @@
             </div>
         </div>
 
+        <div class="grid sm:grid-cols-2 grid-cols-1 gap-5 py-2 ">
         <div class="bg-primary bg-opacity-10 rounded-2xl shadow-lg p-4">
-            <div class="bg-white rounded-2xl p-6">
+            <div class="bg-white rounded-2xl px-4 py-2">
+                <h1 class="my-4 font-poppins font-semibold text-2xl text-center text-primary">Antrean Belum Dipanggil</h1>
                 <table class="table border w-full text-center">
                     <thead class="bg-primary bg-opacity-10">
                         <tr class="font-poppins font-bold text-lg">
                             <th class="px-4 py-2">No. Antrian</th>
                             <th class="px-4 py-2">Panggil</th>
-                            <th class="px-4 py-2">Status</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200">
                         <tr class="font-poppins font-medium text-lg">
-                            <td class="px-4 py-3">TL003</td>
+                            <td class="px-4 py-3">TL005</td>
                             <td class="px-4 py-3">
                                 <div class="bg-primary py-3 rounded-lg flex items-center justify-center gap-2">
                                     <img src="{{asset('icons/microphone-light.svg')}}" alt="Microphone" class="w-3">
                                     <img src="{{asset('icons/rythm.svg')}}" alt="Rythm" class="h-5">
                                 </div>
                             </td>
-                            <td class="px-4 py-3">Belum Selesai</td>
                         </tr>
                         <tr class="font-poppins font-medium text-lg">
-                            <td class="px-4 py-3">TL002</td>
+                            <td class="px-4 py-3">TL006</td>
                             <td class="px-4 py-3">
                                 <div class="bg-primary py-3 rounded-lg flex items-center justify-center gap-2">
                                     <img src="{{asset('icons/microphone-light.svg')}}" alt="Microphone" class="w-3">
                                     <img src="{{asset('icons/rythm.svg')}}" alt="Rythm" class="h-5">
                                 </div>
                             </td>
-                            <td class="px-4 py-3">Selesai</td>
                         </tr>
                         <tr class="font-poppins font-medium text-lg">
-                            <td class="px-4 py-3">TL001</td>
+                            <td class="px-4 py-3">TL007</td>
                             <td class="px-4 py-3">
                                 <div class="bg-primary py-3 rounded-lg flex items-center justify-center gap-2">
                                     <img src="{{asset('icons/microphone-light.svg')}}" alt="Microphone" class="w-3">
                                     <img src="{{asset('icons/rythm.svg')}}" alt="Rythm" class="h-5">
                                 </div>
                             </td>
-                            <td class="px-4 py-3">Selesai</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+        </div>
+        <div class="bg-primary bg-opacity-10 rounded-2xl shadow-lg p-4">
+            <div class="bg-white rounded-2xl px-4 py-2">
+                <h1 class="my-4 font-poppins font-semibold text-2xl text-center text-primary">Antrean Sudah Dipanggil</h1>
+                <table class="table border w-full text-center">
+                    <thead class="bg-primary bg-opacity-10">
+                        <tr class="font-poppins font-bold text-lg">
+                            <th class="px-4 py-2">No. Antrian</th>
+                            <th class="px-4 py-2">No. Counter</th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200">
+                        <tr class="font-poppins font-medium text-lg">
+                            <td class="px-4 py-3">TL004</td>
+                            <td class="px-4 py-3">Counter 1</td>
+                        </tr>
+                        <tr class="font-poppins font-medium text-lg">
+                            <td class="px-4 py-3">TL003</td>
+                            <td class="px-4 py-3">Counter 3</td>
+                        </tr>
+                        <tr class="font-poppins font-medium text-lg">
+                            <td class="px-4 py-3">TL002</td>
+                            <td class="px-4 py-3">Counter 2</td>
+                        </tr>
+                        <tr class="font-poppins font-medium text-lg">
+                            <td class="px-4 py-3">TL001</td>
+                            <td class="px-4 py-3">Counter 1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
         </div>
     </main>
 </body>
